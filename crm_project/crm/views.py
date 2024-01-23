011d50ae3f5a6328c40671537724afc9d473776f
Anurag11L <anuraglade@gmail.com>
2024-01-23 23:29:27 +0530

    ui/ux

--- crm_project/crm/views.py
+++ crm_project/crm/views.py
@@ -2,6 +2,11 @@
 from django.shortcuts import render,redirect
 from .models import Customer
 from django.http import HttpResponse
+from .forms import CustomerForm
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.utils import timezone
+import pytz
 
 def customer_list(request):
     customers = Customer.objects.all()
@@ -15,8 +20,15 @@ def create_customer(request):
         phone = request.POST.get('phone')
         address = request.POST.get('address')
 
-        # Create a new customer in the database
-        Customer.objects.create(name=name, email=email, phone=phone, address=address)
+        # Get the current time in UTC
+        utc_now = timezone.now()
+
+        # Convert UTC time to Indian Standard Time (IST)
+        ist = pytz.timezone('Asia/Kolkata')
+        ist_now = utc_now.astimezone(ist)
+
+        # Create a new customer in the database with IST time
+        Customer.objects.create(name=name, email=email, phone=phone, address=address, created_at=ist_now)
 
         # Redirect to the customer list page after creating a new customer
         return redirect('crm:customer_list')
@@ -24,8 +36,6 @@ def create_customer(request):
     # If the request method is not POST, render the form
     return render(request, 'crm/create_customer.html')
 
-
-from .forms import CustomerForm
 
 def edit_customer(request, customer_id):
     customer = get_object_or_404(Customer, id=customer_id)
@@ -40,8 +50,7 @@ def edit_customer(request, customer_id):
 
     return render(request, 'crm/edit_customer.html', {'form': form, 'customer': customer})
 
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
+
 
 def delete_customer(request, customer_id):
     customer = get_object_or_404(Customer, id=customer_id)
